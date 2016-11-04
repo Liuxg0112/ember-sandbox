b9fd3d4bb385ac6f7ef7f376da1c46c89d41d62e
liuxg <liu.xingguo@qq.com>
2016-11-04 13:45:28 +0800

    done

--- app/modules/components/user-section/detail/component.js
+++ app/modules/components/user-section/detail/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    classNames: ['cell', '-8of12']
+}).reopenClass({
+    positionalParams: ['selectedUser']
 });
--- app/modules/components/user-section/list/component.js
+++ app/modules/components/user-section/list/component.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
-
+import styles from 'sandbox/modules/index/styles';
 export default Ember.Component.extend({
     tagName:'ul',
     classNames:['cell','-4of12'],
     localClassNames:'user-list',
+    styles
+}).reopenClass({
+    positionalParams:['api']
 });
